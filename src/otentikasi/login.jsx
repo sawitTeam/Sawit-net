1ea037415f9c7b444134ef526e9881c62f4de107
019-Mahansa <lightningplay2009@gmail.com>
2026-02-24 11:09:11 +0700

    Add responsive desain(but it's still need to fix)

--- src/otentikasi/login.jsx
+++ src/otentikasi/login.jsx
@@ -13,24 +13,27 @@ export const Login = () =>{
             </div>
 
             <div className="menu">
-                <div className="title">
-                     <h1>Welcome back</h1>
-                </div>
+                <div className="subMenu">
+  
+                    <div className="title">
+                        <h1>Welcome back</h1>
+                    </div>
 
-                <div className="dirrectToSignUp">
-                    <p>Don't have account yet? <a href="#">Sign up</a></p>
-                </div>
+                    <div className="dirrectToSignUp">
+                        <p>Don't have account yet? <a href="#">Sign up</a></p>
+                    </div>
 
-                <div className="form">
-                    <Form text="Email Address"></Form>
-                    <Form text="Password"></Form>
-                </div>
-                <div className="button">
-                    <Button text="Login"></Button>
-                </div>
+                    <div className="form">
+                        <Form text="Email Address"></Form>
+                        <Form text="Password"></Form>
+                    </div>
+                    <div className="button">
+                        <Button text="Login"></Button>
+                    </div>
 
-                <div className="otherChoise">
-
+                    <div className="otherChoise">
+                        <tr></tr>
+                    </div>
                 </div>
 
 
